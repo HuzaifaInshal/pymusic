8366f27ae5749e7c2fbb351344c7b9f928044e18
HuzaifaInshal <huzaifainshal@gmail.com>
2024-03-02 17:02:01 +0500

    'improved_for_vercel'

--- base/views.py
+++ base/views.py
@@ -5,21 +5,39 @@ from django.http import HttpResponse
 import yt_dlp as youtube_dl
 import os
 
+#kXYiU_JCYtU
 
+# def Main(request,musid):
+#     ydl_opts = {
+#             'format': 'bestaudio',
+#         }
 
-def Main(request,musid):
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         try:
+#             info = ydl.extract_info('https://www.youtube.com/watch?v=' + musid, download=True)
+#             filename = ydl.prepare_filename(info)
+#             os.rename(filename, filename[:-4] + "mp3")  # Rename the file by changing extension
+#             with open(filename[:-4] + "mp3", 'rb') as audio_file:
+#                 audio_data = audio_file.read()
+#             os.remove(filename[:-4] + "mp3")  # Remove the temporary file
+#             return HttpResponse(audio_data, content_type='audio/mpeg')
+#         except Exception as e:
+#             return Response({'error': str(e)}, status=500)
+
+def Main(request, musid):
     ydl_opts = {
-            'format': 'bestaudio',
-        }
+        'format': 'bestaudio',
+        'outtmpl': '/tmp/%(title)s.%(ext)s',  # Save file to /tmp directory
+    }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             info = ydl.extract_info('https://www.youtube.com/watch?v=' + musid, download=True)
             filename = ydl.prepare_filename(info)
-            os.rename(filename, filename[:-4] + "mp3")  # Rename the file by changing extension
-            with open(filename[:-4] + "mp3", 'rb') as audio_file:
+            # File already saved in /tmp directory as per outtmpl
+            with open(filename, 'rb') as audio_file:
                 audio_data = audio_file.read()
-            os.remove(filename[:-4] + "mp3")  # Remove the temporary file
+            os.remove(filename)  # Remove the temporary file
             return HttpResponse(audio_data, content_type='audio/mpeg')
         except Exception as e:
             return Response({'error': str(e)}, status=500)
